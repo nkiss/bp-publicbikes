a74461c6cc01ec3156020ca3ccc30c3e3a209591
nkiss <nkiss@users.noreply.github.com>
2025-02-25 18:07:49 +0000

    ✅ Ready to clone and code.

---
+++ bp_publicbikes/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for bp_publicbikes."""
+
+from bp_publicbikes.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- bp_publicbikes/cli.py
+++ bp_publicbikes/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for bp_publicbikes project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m bp_publicbikes` and `$ bp_publicbikes `.
 
     This is your program's entry point.
 
